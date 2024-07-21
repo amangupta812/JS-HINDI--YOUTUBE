bb5ce11723742f170e86e902fd26698e4f395f58
amangupta812 <159109902+amangupta812@users.noreply.github.com>
2024-07-21 12:46:25 +0000

    container config done

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict;" //treat all codes as newer version
+
+//alert(8+7) //we  are using node js not browser
+
+console.log(8+
+    7);// code readability should be high
+
+console.log("aman");
+
+let name= "aman"
+let age = 20
+let isloggedIn = false
+let state;
+
+//number => 2 to the power 53
+//bigint 
+//String => ""
+//boolean => true or false
+//null = standalone value
+//undefined=>
+//symbol => unique
+
+
+//object 
+
+console.log(typeof "aman");
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof null);
